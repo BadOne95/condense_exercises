0683c263308458d7ba2260c4f22e244d0f7c8c68
Bado <francesco.badini@isendu.com>
2023-09-08 00:10:02 +0200

    fix: removed typeorm models

--- src/backend_exercise/dao/index.ts
+++ src/backend_exercise/dao/index.ts
@@ -1,6 +1,8 @@
-import { Category } from "../models/category";
+import { PrismaClient } from "@prisma/client";
 
 export const getAllShoesFromPath = async (path: string) => {
   // effettua una query sulla tabella shoe_to_category
+  const client = new PrismaClient();
+  client;
   return Promise.resolve();
 };
