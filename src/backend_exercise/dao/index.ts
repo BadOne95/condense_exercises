e33f85305da69bbe24fef523c7debbdc442e3c32
Bado <francesco.badini@isendu.com>
2023-09-07 23:49:17 +0200

    fix: typeorm -> prisma

---
+++ src/backend_exercise/dao/index.ts
@@ -0,0 +1,6 @@
+import { Category } from "../models/category";
+
+export const getAllShoesFromPath = async (path: string) => {
+  // effettua una query sulla tabella shoe_to_category
+  return Promise.resolve();
+};
